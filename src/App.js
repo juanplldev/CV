a9fb7a4b9d1024795dc3cf6278da685fda531fb8
JuanPablo-Llorente <juanpablollorentej@gmail.com>
2022-07-13 15:39:04 -0300

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// Dependencies
+import React from "react";
+// import {Route, Routes} from "react-router-dom";
+// Files
+import "./App.css";
+
+
+function App()
+{
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+};
+
+
+export default App;
